8d42525d9724c4ae625bbcf0d77562afb17cce41
Cassandra Parisi <cparisi1290@gmail.com>
2026-05-24 11:40:52 -0600

    fix: resolve Alpine.data registration and add infinite scroll + dots to testimonials

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 import Alpine from "alpinejs";
 import collapse from "@alpinejs/collapse";
 
-Alpine.plugin(collapse);
 window.Alpine = Alpine;
+Alpine.plugin(collapse);
 
-// Import data files so they register before Alpine starts
-import "./projects-data.js";
-import "./testimonials-data.js";
-import "./blog-data.js";
+async function init() {
+  await import("./projects-data.js");
+  await import("./testimonials-data.js");
+  await import("./blog-data.js");
+  Alpine.start();
+}
 
-Alpine.start();
+init();
